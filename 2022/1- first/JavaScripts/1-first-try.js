e368dbb2d09c5bba0618dbc5f7611727f979668f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-11 00:30:43 +0430

    this codes changes

--- 2022/1- first/JavaScripts/1-first-try.js
+++
@@ -1,5 +0,0 @@
-let a = 2
-let b = 3
-let c = a + b
-
-console.log(b)
--- 2022/1- first/JavaScripts/2-Second.js
+++ 2022/1- first/JavaScripts/2-Second.js
@@ -1,1 +1,12 @@
-alert('Wellcome to this site my frind :)');
+
+var Name = "sina lalebakhsh";
+
+var Number = 1371;
+
+var IsExists = true;
+
+
+alert(typeof 'Wellcome to this site my frind :)');
+alert(typeof Name);
+alert(typeof Number);
+alert(typeof IsExists);
