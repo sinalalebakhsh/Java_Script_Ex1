00b7387ee25e8731d799dcd434a3d2a49d6f0022
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-14 01:51:44 +0430

    change all

--- 2022/1- first/JavaScripts/2-Second.js
+++ 2022/1- first/JavaScripts/2-Second.js
@@ -1,21 +1,6 @@
+var x = 10;
 
-var Name = "sina lalebakhsh";
+x++;
 
-var Number = 1371;
-
-var IsExists = true;
-
-
-alert('Wellcome to this site my frind :)');
-
-function SinaAlert() {
-    var yek = 1001;
-    var doo = 30;
-    var seh = yek + doo;
-    alert(seh);
-}
-
-var list_names = ['sina', 'lale', 'mina', 'jale', 'lina'];
-
-document.write('Name: ',list_names[0]);
+document.write(x);
 
---
+++ 2022/1- first/JavaScripts/3-three.js
@@ -0,0 +1,3 @@
+var x = 10;
+document.write(x)
+
