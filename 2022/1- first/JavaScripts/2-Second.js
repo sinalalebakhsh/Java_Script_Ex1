d7f52292ffc30a70644520fa559fd72148d480b3
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-17 23:35:36 +0430

    add codes

---
+++ 2022/1- first/JavaScripts/2-Second.js
@@ -0,0 +1,12 @@
+<script>
+var lbl = document.getElementById("lblResult")
+function Sum() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var sum = num1 + num2;
+    lbl.innerHTML = sum;
+}
+
+</script>
