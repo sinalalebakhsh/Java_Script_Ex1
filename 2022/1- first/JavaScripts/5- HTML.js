96eb3732813ef3def0e22b7c1ec64bb87d80e589
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-20 19:28:04 +0430

    add codes

---
+++ 2022/1- first/JavaScripts/5- HTML.js
@@ -0,0 +1,13 @@
+<script>
+    var person = {
+        name : 'sina',
+        family : 'lalenakhsh',
+        age : 29,
+        fullname : function() {
+            return this.name + " " + this.family;
+        }
+    };
+    
+    console.log(person.fullname());
+
+</script>
