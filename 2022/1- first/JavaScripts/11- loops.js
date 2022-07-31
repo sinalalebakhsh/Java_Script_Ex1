a5e7d9785c1a7cc72716ac6116f93cf9bf385926
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-31 22:05:41 +0430

    About loop setting

--- 2022/1- first/JavaScripts/11- loops.js
+++ 2022/1- first/JavaScripts/11- loops.js
@@ -1,39 +1,10 @@
-switch(new Date().getDay())
-    {
-        case 0:
-            {
-                alert('Sunday');
-                break
-            }
-        case 1:
-            {
-                alert('Monday');
-                break
-            }
-        case 2:
-            {
-                alert('Tuesday');
-                break
-            }
-        case 3:
-            {
-                alert('Wednday');
-                break
-            }
-        case 4:
-            {
-                alert('Thursday');
-                break
-            }
-        case 5:
-            {
-                alert('Friday');
-                break
-            }
-        case 6:
-            {
-                alert('Saturday');
-                break
-            }
+var list_names = ['sina', 'mina', 'lina', 'bina'];
 
-    }
+var element = document.getElementById('result');
+
+var list_names_static = list_names[0] + " <br/>";
+list_names_static += list_names[1] + " <br/>";
+list_names_static += list_names[2] + " <br/>";
+list_names_static += list_names[3] + " <br/>";
+
+element.innerHTML = list_names_static;
