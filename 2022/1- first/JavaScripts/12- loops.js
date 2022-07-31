887015c8e12f0b7d9d17e258018f2cad6d3acd3e
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-31 22:24:58 +0430

    About loop setting i in list_names

---
+++ 2022/1- first/JavaScripts/12- loops.js
@@ -0,0 +1,13 @@
+var list_names = ['sina', 'mina', 'lina', 'bina'];
+
+var element = document.getElementById('result');
+
+var list_names_dynamic = "";
+
+for ( i in list_names )
+{
+    alert(i + " " + list_names[i])    
+}
+// element.innerHTML = list_names_dynamic;
+
+
