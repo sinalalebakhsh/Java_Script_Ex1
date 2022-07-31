c02afe047fa64a54abea8f1b50337895d4502b22
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-31 22:10:26 +0430

    About loop setting

--- 2022/1- first/JavaScripts/11- loops.js
+++ 2022/1- first/JavaScripts/11- loops.js
@@ -2,9 +2,15 @@ var list_names = ['sina', 'mina', 'lina', 'bina'];
 
 var element = document.getElementById('result');
 
-var list_names_static = list_names[0] + " <br/>";
-list_names_static += list_names[1] + " <br/>";
-list_names_static += list_names[2] + " <br/>";
-list_names_static += list_names[3] + " <br/>";
+var list_names_dynamic = "";
 
-element.innerHTML = list_names_static;
+
+
+
+
+// var list_names_static = list_names[0] + " <br/>";
+// list_names_static += list_names[1] + " <br/>";
+// list_names_static += list_names[2] + " <br/>";
+// list_names_static += list_names[3] + " <br/>";
+// element.innerHTML = list_names_static;
+
