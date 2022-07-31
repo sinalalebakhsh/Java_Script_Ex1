2099185fb172786e81ea17de868a1c0dc2e53652
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-31 20:53:38 +0430

    add codes

---
+++ 2022/1- first/JavaScripts/9- for 9 html .js
@@ -0,0 +1,24 @@
+
+<script>
+
+        var x = "xx";
+
+        switch(x)
+        {
+                case 0:
+                {
+                        alert("x is 0");
+                }
+                case 1:
+                {
+                        alert("x is 1");
+                }
+                default:
+                {
+                        alert("x is'n found ");
+
+                }
+        }
+
+
+</script>
