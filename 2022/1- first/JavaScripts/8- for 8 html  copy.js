77150f852525a8ace6978258e4ae6c968e6760cd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-07-31 20:36:50 +0430

    About swith and case

---
+++ 2022/1- first/JavaScripts/8- for 8 html  copy.js
@@ -0,0 +1,15 @@
+
+<script>
+
+    var morning = 6;
+    var noon = 12;
+    var night = 21;
+    var time = Number;
+
+    if (time == morning) {
+        alert("True ###");
+} else if (time == Number) {
+        alert("time == Number");
+}
+
+</script>
